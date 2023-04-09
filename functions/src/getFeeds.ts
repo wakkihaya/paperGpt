9085524b4de8308d84858bd60fc9e29d8e8fcddf
wakkihaya <wakkihaya@gmail.com>
2023-04-09 15:17:34 +0200

    fix: getFeeds

--- functions/src/getFeeds.ts
+++ functions/src/getFeeds.ts
@@ -1,17 +1,13 @@
 import * as functions from 'firebase-functions';
-import axios from 'axios';
+import { search } from 'scholarly';
 
 // @link: Scholary api : https://github.com/ukalwa/scholarly
 export const getFeeds = functions.https.onRequest(async (request, response) => {
-  const query = request.query.q; // Example query parameter
-  const url = `https://api.scholarly.pub/search?q=${query}`;
-  const options = {
-    headers: { 'User-Agent': 'Firebase Cloud Functions' },
-  };
+  const query = request.query.q as string; // Example query parameter
 
   try {
-    const apiResponse = await axios.get(url, options);
-    response.send(apiResponse.data);
+    const apiResponse = await search(query);
+    response.send(apiResponse);
   } catch (error) {
     console.error(error);
     response.status(500).send('Error calling Scholarly API.');
